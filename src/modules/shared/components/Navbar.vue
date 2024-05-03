56cb0155a08d6c351c1c9a4fc7f575fd4315c49b
Roberto-Irigoyen-ou <roberto.irigoyen@oubita.com>
2024-05-03 15:12:40 +0200

    Cambios por parametro y route

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <router-link to="/">Pokemon List</router-link>
-    <router-link to="/id">Pokemon por id</router-link>
+    <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }"
+      >Pokemon por id</router-link
+    >
     <router-link to="/about">Pokemon About</router-link>
   </div>
 </template>
--- src/router/router.js
+++ src/router/router.js
@@ -16,11 +16,16 @@ const routes = [
       )
   },
   {
-    path: '/id',
+    path: '/:id',
+    name: 'pokemon-id',
     component: () =>
       import(
         /* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'
-      )
+      ),
+    props: route => {
+      const id = Number(route.params.id)
+      return isNaN(id) ? { id: 1 } : { id: id }
+    }
   },
   {
     path: '/:pathMatch(.*)*',
