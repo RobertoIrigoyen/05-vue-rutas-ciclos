1f44c85483c0ac07b790d7e8518b6683666a2e5d
Irigoyen López-Baisson <ririgoyen@indra.es>
2024-05-13 13:54:50 +0200

    Routerlink personalizado

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,12 +1,32 @@
 <template>
   <div>
-    <router-link to="/">Pokemon List</router-link>
+    <!--     <router-link :to="{ name: 'pokemon-home' }">Pokemon List</router-link>
     <router-link :to="{ name: 'pokemon-id', params: { id: 85 } }"
       >Pokemon por id</router-link
     >
-    <router-link to="/about">Pokemon About</router-link>
+    <router-link :to="{ name: 'pokemon-about' }">Pokemon About</router-link> -->
+
+    <CustomLink v-for="link in links" :key="link.to" :link="link" />
   </div>
 </template>
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  components: {
+    CustomLink: defineAsyncComponent(() => import("./CustomLink.vue")),
+  },
+  data() {
+    return {
+      links: [
+        { to: "/home", name: "Pokemons" },
+        { to: "/pokemondid/151", name: "Pokemons por id" },
+        { to: "/about", name: "Pokemons About" },
+        { to: "https://www.google.es/", name: "Google" },
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 div {
@@ -18,7 +38,7 @@ div a {
   color: #2c3e50;
   margin: 0 10px;
 }
-a.router-link-exact-active {
+/* a.router-link-exact-active {
   color: #42b983;
-}
+} */
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,44 +1,50 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-const routes = [
-  {
-    path: '/',
-    component: () =>
-      import(
-        /* webpackChunkName: "ListPage" */ '@/modules/pokemon/pages/ListPage'
-      )
-  },
-  {
-    path: '/about',
-    component: () =>
-      import(
-        /* webpackChunkName: "AboutPage" */ '@/modules/pokemon/pages/AboutPage'
-      )
-  },
-  {
-    path: '/:id',
-    name: 'pokemon-id',
-    component: () =>
-      import(
-        /* webpackChunkName: "PokemonPage" */ '@/modules/pokemon/pages/PokemonPage'
-      ),
-    props: route => {
-      const id = Number(route.params.id)
-      return isNaN(id) ? { id: 1 } : { id: id }
+const routes = [{
+        path: '/',
+        redirect: '/home'
+    },
+    {
+        path: '/home',
+        name: 'pokemon-home',
+        component: () =>
+            import (
+                /* webpackChunkName: "ListPage" */
+                '@/modules/pokemon/pages/ListPage'
+            )
+    },
+    {
+        path: '/about',
+        name: 'pokemon-about',
+        component: () =>
+            import (
+                /* webpackChunkName: "AboutPage" */
+                '@/modules/pokemon/pages/AboutPage'
+            )
+    },
+    {
+        path: '/pokemondid/:id',
+        name: 'pokemon-id',
+        component: () =>
+            import (
+                /* webpackChunkName: "PokemonPage" */
+                '@/modules/pokemon/pages/PokemonPage'
+            ),
+        props: route => {
+            const id = Number(route.params.id)
+            return isNaN(id) ? { id: 1 } : { id: id }
+        }
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        component: () =>
+            import ( /* webpackChunkName: "NotPageFound" */ '@/modules/shared/pages/NotPageFound')
     }
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    component: () =>
-      import(
-        /* webpackChunkName: "NotPageFound" */ '@/modules/shared/pages/NotPageFound'
-      )
-  }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+    history: createWebHashHistory(),
+    routes
 })
 
 export default router
